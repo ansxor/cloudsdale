9486ebe3a586d43d293a497a074c681e01f012a5
answer <darienreese@gmail.com>
2024-09-03 17:49:53 -0400

    Added Samba configuration

---
+++ services/samba.nix
@@ -0,0 +1,32 @@
+{ config, ... }
+{
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = smbnix
+      netbios name = smbnix
+      security = user 
+      #use sendfile = yes
+      #max protocol = smb2
+      # note: localhost is the ipv6 localhost ::1
+      hosts allow = 192.168.0. 127.0.0.1 localhost
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+    shares = {
+      answer = {
+        path = "/home/answer/share";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "answer";
+      };
+    };
+  };
+}
