e291bf54816dd7e1ac766a9567874aba32ab22a4
answer <darienreese@gmail.com>
2024-09-04 12:32:29 -0400

    ContentAPI Package finally builds

--- packages/contentapi.nix
+++ packages/contentapi.nix
@@ -12,20 +12,22 @@ buildDotnetModule rec {
   src = fetchFromGitHub {
     owner = "randomouscrap98";
     repo = "contentapi";
-    sha256 = "7d5dbce89face1addea4da75cf7457c53b6a47f9";
+    rev = "7d5dbce89face1addea4da75cf7457c53b6a47f9";
+    hash = "sha256-oQ2xQL8UlrOvEJwXOBtAKuZyUtuFjk7D2G0npT+t2WQ=";
   };
 
-  propagatedBuildInputs = [ sqlite ];
+  runtimeDeps = [ sqlite ];
 
   projectFile = "contentapi/contentapi.csproj";
 
-  outputBin = "publish";
-  framework = "net6.0";
-
   nugetDeps = ./deps/contentapi.nix;
+
+  dotnet-sdk = dotnetCorePackages.sdk_6_0;
+  dotnet-runtime = dotnetCorePackages.aspnetcore_6_0;
 
   meta = with lib; {
     description = "ContentAPI";
     license = licenses.mit;
+    platforms = platforms.linux;
   };
 }
