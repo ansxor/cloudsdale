959fa2f774c0cc37218f467798e2fc4cacbaf570
answer <darienreese@gmail.com>
2024-09-04 12:45:37 -0400

    Postinstall script pls work

--- packages/contentapi.nix
+++ packages/contentapi.nix
@@ -3,7 +3,9 @@
   dotnetCorePackages,
   buildDotnetModule,
   sqlite,
-  lib
+  lib,
+  writeShellScript,
+  bash
 }:
 
 buildDotnetModule rec {
@@ -17,6 +19,7 @@ buildDotnetModule rec {
   };
 
   runtimeDeps = [ sqlite ];
+  nativeBuildInputs = [ sqlite ];
 
   projectFile = "contentapi/contentapi.csproj";
 
@@ -30,4 +33,51 @@ buildDotnetModule rec {
     license = licenses.mit;
     platforms = platforms.linux;
   };
+
+  
+  # Add the migration script
+  migrationScript = writeShellScript "run-migrations.sh" ''
+    set -e
+
+    DB=''${1:-content.db}
+    BACKUP=''${2:-content.db.bak}
+    DBMIGRATIONS=''${3:-dbmigrations}
+
+    DB_DIR=$(dirname $DB)
+
+    # Make a backup of the current db, if it exists
+    if [ -e "$DB" ]
+    then 
+       rm -f "$BACKUP"
+       echo "Backing up db to $BACKUP"
+       sqlite3 "$DB" ".backup $BACKUP"
+    fi
+
+    for f in $DBMIGRATIONS/*.sql
+    do
+       df="''${DB_DIR}/$(basename "$f").done"
+       if [ -r $df ]
+       then
+          continue
+       fi
+       echo "Processing $f..."
+       cat $f | sqlite3 "$DB"
+       touch $df
+    done
+  '';
+
+  # Add a postInstall phase to run the migrations
+  postInstall = ''
+    mkdir -p $out/bin
+    cp ${migrationScript} $out/bin/run-migrations.sh
+    chmod +x $out/bin/run-migrations.sh
+
+    # Run the migrations
+    DB_LOCATION=$out/share/${name}
+    mkdir -p $DB_LOCATION
+    ${bash}/bin/bash $out/bin/run-migrations.sh \
+      $DB_LOCATION/content.db \
+      $DB_LOCATION/content.db.bak \
+      $src/Deploy/dbmigrations
+  '';
 }
